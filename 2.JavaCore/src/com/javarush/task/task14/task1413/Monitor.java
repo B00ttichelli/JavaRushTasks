c1e3fc425db31662844ea20d98bf2152826b3b6b
B00ttichelli <donttrytofish@protonmail.com>
2021-06-17 15:00:47 +0300

    Initial commit

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1413/Monitor.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task14.task1413;
+
+public class Monitor implements CompItem{
+    private String name = "Monitor";
+    @Override
+    public String getName() {
+        return name;
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task14/task1413/Mouse.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task14.task1413;
+
+public class Mouse implements CompItem{
+    private String name = "Mouse";
+    @Override
+    public String getName() {
+        return name;
+    }
+}
