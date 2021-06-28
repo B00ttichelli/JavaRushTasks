07c4429a60daf3cd6048e35ba4bd7ff0b10fce47
B00ttichelli <donttrytofish@protonmail.com>
2021-06-28 10:52:49 +0300

    Initial commit

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -0,0 +1,75 @@
+package com.javarush.task.task17.task1721;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/* 
+Транзакционность
+
+Транзакционность
+Сделать метод joinData транзакционным, т.е. если произошел сбой, то данные не должны быть изменены.
+1. Считать с консоли 2 имени файла.
+2. Считать построчно данные из файлов. Из первого файла - в allLines, из второго - в forRemoveLines.
+В методе joinData:
+3. Если список allLines содержит все строки из forRemoveLines, то удалить из списка allLines все строки, которые есть в forRemoveLines.
+4. Если условие из п.3 не выполнено, то:
+4.1. очистить allLines от данных
+4.2. выбросить исключение CorruptedDataException
+Метод joinData должен вызываться в main. Все исключения обработайте в методе main.
+Не забудь закрыть потоки.
+
+
+Requirements:
+1. Класс Solution должен содержать public static поле allLines типа List<String>.
+2. Класс Solution должен содержать public static поле forRemoveLines типа List<String>.
+3. Класс Solution должен содержать public void метод joinData() который может бросать исключение CorruptedDataException.
+4. Программа должна считывать c консоли имена двух файлов.
+5. Программа должна считывать построчно данные из первого файла в список allLines.
+6. Программа должна считывать построчно данные из второго файла в список forRemoveLines.
+7. Метод joinData должен удалить в списке allLines все строки из списка forRemoveLines, если в allLines содержаться все строки из списка forRemoveLines.
+8. Метод joinData должен очистить список allLines и выбросить исключение CorruptedDataException, если в allLines не содержаться все строки из списка forRemoveLines.
+9. Метод joinData должен вызываться в main.
+*/
+
+public class Solution {
+    public static List<String> allLines = new ArrayList<String>();
+    public static List<String> forRemoveLines = new ArrayList<String>();
+
+    public static void main(String[] args) throws IOException {
+
+
+        Solution solution = new Solution();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        allLines = Files.newBufferedReader(Paths.get(bufferedReader.readLine())).lines().collect(Collectors.toList());
+        forRemoveLines = Files.newBufferedReader(Paths.get(bufferedReader.readLine())).lines().collect(Collectors.toList());
+        try {
+            solution.joinData();
+        } catch (CorruptedDataException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public  void joinData() throws IOException,CorruptedDataException {
+
+
+        if (allLines.containsAll(forRemoveLines)){
+            allLines.removeAll(forRemoveLines);
+        }else {
+            allLines.clear();
+            throw new CorruptedDataException();
+        }
+
+
+
+    }
+}
