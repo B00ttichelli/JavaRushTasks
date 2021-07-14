58ef47679d8ed74dafad14df0389049a57a66b9e
B00ttichelli <donttrytofish@protonmail.com>
2021-07-14 14:09:06 +0300

    Initial commit

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -0,0 +1,73 @@
+package com.javarush.task.task19.task1922;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Ищем нужные строки
+
+Ищем нужные строки
+Считать с консоли имя файла.
+Вывести в консоль все строки из файла, которые содержат всего 2 слова из списка words.
+Закрыть потоки.
+
+Пример:
+words содержит слова А, Б, В
+
+Строки:
+В Б А Д //3 слова из words, не подходит
+А Б А Д //3 слова из words, не подходит
+Д А Д //1 слово из words, не подходит
+Д А Б Д //2 слова - подходит, выводим
+Д А А Д //2 слова - подходит, выводим
+
+
+Requirements:
+1. Класс Solution должен содержать публичное статическое поле words типа List<String>, которое должно быть сразу проинициализировано.
+2. Класс Solution должен содержать статический блок, в котором добавляются три или больше слов в список words.
+3. Программа должна считывать имя файла с консоли (используй BufferedReader).
+4. BufferedReader для считывания данных с консоли должен быть закрыт.
+5. Программа должна считывать содержимое файла (используй FileReader).
+6. Поток чтения из файла (FileReader) должен быть закрыт.
+7. Программа должна выводить в консоль все строки из файла, которые содержат всего 2 слова из списка words.
+*/
+
+public class Solution {
+    public static List<String> words = new ArrayList<String>();
+
+    static {
+        words.add("файл");
+        words.add("вид");
+        words.add("В");
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader file = new BufferedReader(new FileReader(bufferedReader.readLine()));
+        bufferedReader.close();
+        while (file.ready()){
+            String tmp  = file.readLine();
+            int counter  = 0;
+            String[] split = tmp.split(" ");
+            for (String word: words
+                 ) {
+                for (String splited:split
+                     ) {
+                    if(word.equals(splited)){
+                        counter++;
+                    }
+                }
+            }
+            if(counter == 2){
+                System.out.println(tmp);
+            }
+        }
+        file.close();
+
+    }
+}
